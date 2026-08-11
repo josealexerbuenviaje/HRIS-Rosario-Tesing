0573f2836ca3d03933cc08691bc8e2096f37384c
josealexerbuenviaje <josealexerbuenviaje@gmail.com>
2026-08-11 11:37:59 +0800

    Add Vercel origin to CORS allowlist

--- backend/cors.php
+++ backend/cors.php
@@ -5,7 +5,8 @@
 
 $allowed_origins = [
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+    "https://hris-rosario-tesing.vercel.app",
 ];
 
 if (!empty($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins, true)) {
@@ -17,7 +18,6 @@ header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Credentials: true");
 header("Content-Type: application/json; charset=UTF-8");
 
-// Preflight request
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
